f56167c0830dd61102c930f035d5ddd66bfc7835
therajanmaurya <rajanmaurya154@gmail.com>
2016-05-02 15:49:43 +0530

    fragment setup and utils setp

---
+++ app/src/main/java/opensource/popularmoviesstage1/ui/main/MainFragment.java
@@ -0,0 +1,38 @@
+package opensource.popularmoviesstage1.ui.main;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import opensource.popularmoviesstage1.R;
+
+/**
+ * Created by RajanMaurya on 02/05/16.
+ */
+public class MainFragment extends Fragment{
+
+
+    public static MainFragment newInstance() {
+        MainFragment fragment = new MainFragment();
+        Bundle args = new Bundle();
+        //args.putInt("type", type);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        return rootView;
+    }
+}
